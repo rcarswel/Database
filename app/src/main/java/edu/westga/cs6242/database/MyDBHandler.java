d7686ef8d9035dade351c8702b302153bf4f2dce
Robert Carswell <rcarswel@westga.edu>
2016-02-23 15:57:35 -0500

    The Delete Handler Method

--- app/src/main/java/edu/westga/cs6242/database/MyDBHandler.java
+++ app/src/main/java/edu/westga/cs6242/database/MyDBHandler.java
@@ -73,4 +73,27 @@ public class MyDBHandler extends SQLiteOpenHelper {
         return product;
     }
 
+    public boolean deleteProduct(String productname) {
+
+        boolean result = false;
+
+        String query = "Select * FROM " + TABLE_PRODUCTS + " WHERE " + COLUMN_PRODUCTNAME + " =  \"" + productname + "\"";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor cursor = db.rawQuery(query, null);
+
+        Product product = new Product();
+
+        if (cursor.moveToFirst()) {
+            product.setID(Integer.parseInt(cursor.getString(0)));
+            db.delete(TABLE_PRODUCTS, COLUMN_ID + " = ?",
+                    new String[]{String.valueOf(product.getID())});
+            cursor.close();
+            result = true;
+        }
+        db.close();
+        return result;
+    }
+
 }
