f5fae4c666725aa934232ec9d6749ac0ac90b595
Robert Carswell <rcarswel@westga.edu>
2016-02-23 15:15:11 -0500

    Initial commit

---
+++ app/src/main/java/edu/westga/cs6242/database/DatabaseActivity.java
@@ -0,0 +1,13 @@
+package edu.westga.cs6242.database;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DatabaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database);
+    }
+}
